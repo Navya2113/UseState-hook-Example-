dcfcb0f2c3d7ba06e47abb2ad926caa576ea3365
Navya2113 <190684818+Navya2113@users.noreply.github.com>
2025-08-24 18:32:01 +0530

    Add files via upload

---
+++ MyComponent.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function MyComponent() {
+
+  const [name, setName] = useState("Guest");
+  const [age, setAge] = useState(0);
+  const [isEmployed, setIsEmployed] = useState(true);
+  const updateName = () => {
+    setName("Navya");
+  }
+
+  const incrementAge = () => {
+    setAge(age + 5);
+
+  }
+
+  const toggleEmployedStatus = () => {
+    setIsEmployed(!isEmployed);
+  }
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <button onClick={updateName}>Set Name</button>
+
+      <p>Age: {age}</p>
+      <button onClick={incrementAge}>Increment Age</button>
+
+      <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
+      <button onClick={toggleEmployedStatus}>Toggle Status</button>
+    </div>);
+}
+export default MyComponent
